dc66bd0b30c694609b8317b3e5a86f7ebebd32d3
unknown <shubhamgoel23>
2019-07-13 15:17:34 +0530

    modified service to connect with external api to fetch movie details

--- src/main/java/io/imdb/movieinfoservice/MovieInfoServiceApplication.java
+++ src/main/java/io/imdb/movieinfoservice/MovieInfoServiceApplication.java
@@ -3,10 +3,19 @@ package io.imdb.movieinfoservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class MovieInfoServiceApplication {
+
+	@Lazy
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieInfoServiceApplication.class, args);
--- src/main/java/io/imdb/movieinfoservice/resources/MovieResource.java
+++ src/main/java/io/imdb/movieinfoservice/resources/MovieResource.java
@@ -1,18 +1,33 @@
 package io.imdb.movieinfoservice.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import io.imdb.movieinfoservice.models.Movie;
+import io.imdb.movieinfoservice.models.MovieSummary;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
-	
+
+	@Value("${api.key}")
+	private String apiKey;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
 	@RequestMapping("/{movieId}")
 	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
-		return new Movie(movieId, "test name");
+
+		MovieSummary movieSummary = restTemplate.getForObject(
+				"https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey, MovieSummary.class);
+
+		return new Movie(movieSummary.getTitle(), movieSummary.getOverview());
+		// return new Movie(movieId, "test name");
 	}
 
 }
